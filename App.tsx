a94c79650ba0c272743300f5309092626baa364c
b-tanvir <b_tanvir@outlook.com>
2022-08-15 12:44:24 +0600

    basic configs

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const displayName = (guy: number) => {
+    console.log('Hello world');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Finally found that. Do you have anything to say</Text>
       <StatusBar style="auto" />
     </View>
   );
